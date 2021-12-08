9463db7f678209a03b0de658197e72248c52c4e1
James Milne <jmilne22@gmail.com>
2021-12-08 13:48:40 +0200

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,9 @@
+alias nv="nvim"
+alias g="git"
+alias t="terraform"
+alias k="kubectl"
+alias gcb="git checkout -b"
+alias gp="git push"
+alias gc="git checkout"
+alais gc="git commit -m"
+status --is-interactive; and source (jump shell fish | psub)
