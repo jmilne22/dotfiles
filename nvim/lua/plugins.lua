5938fb3e07fec3e6653c28ab67a2a293b8fa5b03
James Milne <jmilne22@gmail.com>
2022-10-23 00:38:10 +0300

    indent

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -10,13 +10,13 @@ return require('packer').startup(function(use)
   use { 'tpope/vim-fugitive' }                       -- git integration
   use { 'junegunn/gv.vim' }                          -- commit history
   use { 'windwp/nvim-autopairs' }                    -- auto close brackets, etc.
-  use { 'nvim-tree/nvim-tree.lua' }					         -- Tree thing
+  use { 'nvim-tree/nvim-tree.lua' }					 -- Tree thing
   use { 'nvim-tree/nvim-web-devicons' }              -- Optional, for file icons
   use { 'vim-airline/vim-airline' }                  -- Status bar
-  use { 'neoclide/coc.nvim' }						             -- Auto Complete
+  use { 'neoclide/coc.nvim' }			             -- Auto Complete
   use { 'terryma/vim-multiple-cursors' }             -- Multiple Cursors
-  use { 'tpope/vim-surround' }						           -- Surround with quotes
-  use { 'mhinz/vim-startify' }						           -- Start Screen
+  use { 'tpope/vim-surround' }					     -- Surround with quotes
+  use { 'mhinz/vim-startify' }					     -- Start Screen
   use { 'ryanoasis/vim-devicons' }                   -- Developer Icons
 
 end)
